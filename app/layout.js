e5c0edb4500e2e746fef8f3ce3dd5bc4c8be9028
NeoPhetoane <phetoaneneo4@gmail.com>
2024-09-11 11:27:34 +0200

    Colors changed in layout and loading page added.

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-900">
-        <header className="bg-red-900 shadow p-4">
+      <body className="bg-gray-100">
+        <header className="bg-gray-300 shadow p-4">
           <h1 className="text-2xl font-bold text-center">E-commerce Store</h1>
         </header>
         <main className="container mx-auto mt-8 p-4">{children}</main>
