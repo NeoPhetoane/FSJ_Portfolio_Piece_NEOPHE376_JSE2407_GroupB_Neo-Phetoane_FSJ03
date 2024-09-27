aada22e816cf47a637a9a30c1885445a6eb555cc
NeoPhetoane <phetoaneneo4@gmail.com>
2024-09-27 11:06:32 +0200

    Search, sort and filter added to Pagination so it applies to them.

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -1,16 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Pagination({ currentPage }) {
+export default function Pagination({
+  currentPage,
+  searchQuery,
+  category,
+  sortBy,
+  order,
+}) {
   const prevPage = currentPage > 1 ? currentPage - 1 : null;
   const nextPage = currentPage + 1;
+  const query = searchQuery ? `&search=${encodeURIComponent(searchQuery)}` : "";
+  const categoryQuery = category
+    ? `&category=${encodeURIComponent(category)}`
+    : "";
+  const sortByQuery = sortBy ? `&sortBy=${encodeURIComponent(sortBy)}` : "";
+  const orderQuery = order ? `&order=${encodeURIComponent(order)}` : "";
 
   return (
     <div className="flex justify-between items-center mt-8">
       {prevPage && (
-        <Link href={`/products?page=${prevPage}`} className="bg-gray-300 px-4 py-2 rounded">Previous
+        <Link
+          href={`/products?page=${prevPage}${query}${categoryQuery}${sortByQuery}${orderQuery}`}
+          className="bg-gray-300 px-4 py-2 rounded"
+        >
+          Previous
         </Link>
       )}
-      <Link href={`/products?page=${nextPage}`} className="bg-gray-300 px-4 py-2 rounded">Next
+      <Link
+        href={`/products?page=${nextPage}${query}${categoryQuery}${sortByQuery}${orderQuery}`}
+        className="bg-gray-300 px-4 py-2 rounded"
+      >
+        Next
       </Link>
     </div>
   );
